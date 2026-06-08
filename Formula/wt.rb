40fbf69c7c4490c27de9ab35f9c84852dd2880ce
robert-brunson-fragile <siinspam@gmail.com>
2026-06-08 16:54:10 -0700

    Update Homebrew formula to v0.1.6

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,8 +1,8 @@
 class Wt < Formula
   desc "Small Bun CLI for managing Git worktrees"
   homepage "https://github.com/QuillDev/wt"
-  url "https://github.com/QuillDev/wt/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "cc44f48714d2ce92b1c527d18f02982e78cb27be09b2876cbb653eeef564a286"
+  url "https://github.com/QuillDev/wt/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "1e5a500130c06489b18b0728a226df988d9a5fb8bf2d31bf0b647c60b9b8d3a7"
   license :cannot_represent
   head "https://github.com/QuillDev/wt.git", branch: "master"
 
